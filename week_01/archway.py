9c1103b7af2e50619655469a7f850297fc3516ca
Surajit Bose <verbose@duck.com>
2025-04-25 12:17:28 -0700

    Add week 1 solutions

---
+++ week_01/archway.py
@@ -0,0 +1,53 @@
+"""
+archway.py
+
+Get Karel over an archway
+
+Programmer: Surajit A. Bose, Date: April 25, 2025
+"""
+
+from karel.stanfordkarel import *
+
+def main():
+    """
+    Move Karel from one corner of the world to the other over an archway
+    
+    Preconditions: 
+    - Karel is on the bottom row leftmost corner facing east
+    - There is an archway three squares high blocking Karel's way forward
+    
+    Postconditions:
+    - Karel is on the bottom row rightmost corner facing east 
+    - Archway is behind Karel
+    """
+    turn_left()
+    move_forward_three()
+    turn_right()
+    move_forward_three()
+    turn_right()
+    move_forward_three()
+    turn_left()
+
+    
+def turn_right():
+    """Enable Karel to face the direction to the right of the current direction
+    
+    Preconditions: Karel is on a corner facing a particular direction
+    
+    Postconditions: 
+    - Karel is on the same corner 
+    - Karel is facing the direction to the right of the previous direction
+    """
+    turn_left()
+    turn_left()
+    turn_left()
+    
+
+def move_forward_three():
+    """Move Karel forward three squares at once()"""
+    move()
+    move()
+    move()
+    
+if __name__ == "__main__":
+    main()
---
+++ week_01/move_beeper_with_fun.py
@@ -0,0 +1,47 @@
+"""
+main.py
+
+Karel moves a beeper from the bottom row to the top row
+
+Programmer: Surajit A Bose, Date: January 6, 2025
+"""
+
+from karel.stanfordkarel import *
+
+def main():
+    """
+    Move Karel and beeper to top row
+    
+    Preconditions:
+    - Karel is in the bottom left corner, facing east
+    - There is a beeper immediately in front of Karel
+
+    Postconditions:
+    - Karel is in the top right corner, facing east
+    - Beeper has been moved and is immediately behind Karel
+    """
+    move()
+    pick_beeper()
+    turn_left()
+    move()
+    move()
+    turn_right()
+    put_beeper()
+    move()
+
+def turn_right():
+    """Enable Karel to face the direction to the right of the current direction
+    
+    Preconditions: Karel is on a corner facing a particular direction
+    
+    Postconditions: 
+    - Karel is on the same corner 
+    - Karel is facing the direction to the right of the previous direction
+    """
+    turn_left()
+    turn_left()
+    turn_left()
+
+# There is no need to edit code beyond this point
+if __name__ == '__main__':
+    main()
