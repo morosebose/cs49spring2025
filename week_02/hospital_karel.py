73c8cc431ff0cc379c96f3839fc355c427c744a2
Surajit Bose <verbose@duck.com>
2025-05-02 14:21:55 -0700

    Refine Hospital Karel solution

--- week_02/hospital_karel.py
+++ week_02/hospital_karel.py
@@ -5,7 +5,7 @@ Karel begins at the left end of a row that has beepers at various points.
 The beepers indicate spots where a hospital should be built.
 Hospitals are two adjacent columns of three beepers each.
 
-Programmer: Surajit A. Bose, Date: 2024.04.26
+Programmer: Surajit A. Bose, Date: 2025.05.02
 """
 
 from karel.stanfordkarel import *
@@ -14,39 +14,31 @@ def main():
     """
     Walk the row and build hospitals where indicated.
     
-    Preconditions: Karel is at one end of the row, facing east.
-        Beepers on the row indicate future hospital locations.
+    Preconditions: 
+    - Karel is at one end of the row, facing east.
+    - Beepers on the row indicate future hospital locations.
         
     Postconditions: Hospitals built.
         Karen is at the other end of the row, facing east.
     """
     while front_is_clear():
-        move_to_supplies()
-        build_hospital()
+        if beepers_present():
+            build_hospital()
         if front_is_clear():
             move()
-            
-def move_to_supplies():
-    """
-    Move Karel to location of hospital.
-    
-    Preconditions: Karel is on a corner with no supplies (beeper), facing east
-    
-    Postcondition: Karel has moved to a corner with a beeper and picked the beeper.
-        Karel is facing east.
-    """
-    while no_beepers_present():
-        move()
-    pick_beeper()
+
 
 def build_hospital():
     """
     Build hospital at current corner.
     
-    Preconditions: Karel is on a corner where a hospital is to be built.
-        Karel is facing east.
+    Preconditions: 
+    - Karel is on a corner with a beeper indicating a hospital site
+    - Karel is facing east
     
-    Postconditions: Karel is on the west corner of the built hospital, facing east.
+    Postconditions: 
+    - Hospital is built
+    - Karel is on the west corner of the built hospital, facing east
     """
     turn_left()
     build_column()
@@ -56,23 +48,31 @@ def build_hospital():
     build_column()
     turn_left()
 
+
 def build_column():
     """
     Build a single column of beepers.
     
-    Preconditions: Karel is correctly oriented to place three consecutive beepers.
+    Preconditions: 
+    - Karel is correctly oriented to build a column of three consecutive beepers.
+    - There may or may not be a beeper on the corner where Karel is
     
-    Postconditions: Karel has placed the three beepers. 
+    Postconditions: 
+    - Column is built
+    - Karel is on top of the third beeper put down, facing the same direction 
     """
-    for i in range(2):
+    if no_beepers_present():
         put_beeper()
+    for i in range(2):
         move()
-    put_beeper()
+        put_beeper()
+
 
 def turn_right():
     """Turn Karel to face right from its original direction."""
     for i in range(3):
         turn_left()
+
 
 if __name__ == '__main__':
     main()
